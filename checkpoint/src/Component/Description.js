bb019f89d1edac74bfcd5879a1fbc5636ad41ae1
cheikhou Diedhiou <cheikhoudiedhiou43@gmail.com>
2023-08-29 17:51:29 +0000

    Third commit

--- checkpoint/src/Component/Description.js
+++
@@ -1,9 +0,0 @@
-import produit from '../product'
-const Description = () => {
-    return ( <div>
-                {produit.description}
-            </div> 
-    );
-}
- 
-export default Description;
